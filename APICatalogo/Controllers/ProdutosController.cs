673011e84f57dad8c94c5a100edb8234e3d600c4
Rafael Kowalski <rafaelmk06@gmail.com>
2022-08-18 21:47:51 -0300

    Criado o controller ProdutosController

---
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -0,0 +1,18 @@
+﻿using APICatalogo.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICatalogo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
