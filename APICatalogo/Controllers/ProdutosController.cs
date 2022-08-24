a9f4de6d8655a1f2fd29d84a60d0a70082f814e3
Rafael Kowalski <rafaelmk06@gmail.com>
2022-08-23 23:13:31 -0300

    Atualizando identação

--- APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -29,7 +29,7 @@ namespace APICatalogo.Controllers
             return produtos;
         }
 
-        [HttpGet("{id:int}", Name="ObterProduto")]
+        [HttpGet("{id:int}", Name = "ObterProduto")]
         public ActionResult<Produto> Get(int id)
         {
             var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
@@ -48,14 +48,14 @@ namespace APICatalogo.Controllers
             _context.Produtos.Add(produto);
             _context.SaveChanges();
 
-            return new CreatedAtRouteResult("ObterProduto", new {id = produto.ProdutoId}, produto);
+            return new CreatedAtRouteResult("ObterProduto", new { id = produto.ProdutoId }, produto);
         }
 
         [HttpPut("{id:int}")]
 
         public ActionResult Put(int id, Produto produto)
         {
-            if (id!=produto.ProdutoId)
+            if (id != produto.ProdutoId)
                 return BadRequest();
 
             _context.Entry(produto).State = EntityState.Modified;
